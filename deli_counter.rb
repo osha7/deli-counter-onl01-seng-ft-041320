cefcd2ff3e21897e7117f72fa0aaad714b7bfd1c
Osha G <ogroetz@gmail.com>
2020-04-16 04:14:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,10 +3,11 @@
 def line(deli)
   if deli.length == 0
   puts "The line is currently empty."
-  elsif deli.length > 0 
+  else deli.length > 0 
     new_array = ["The line is currently:"]
     deli.each_with_index do |name, index|
       new_array << "#{index +1}. #{name}"
 end
   puts new_array.join(" ")
+end
 end
