15e17c56218eeffb4e8d8e418ca750d355410fd7
Osha G <ogroetz@gmail.com>
2020-04-16 04:19:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,3 +16,6 @@ def take_a_number(deli,name)
   deli << name
   puts "Welcome, #{name}. You are number #{deli.length} in line."
 end
+
+def now_serving(deli)
+  if deli.length == log10puts "There is nobody waiting to be served!"
