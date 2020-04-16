0cc0217cd670dc7d8264ad0e61f64b9503c94035
Osha G <ogroetz@gmail.com>
2020-04-16 04:01:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 # Write your code here.
 
 def line(deli)
-  if deli.length == 0
+  deli.length == 0
   puts "The line is currently empty."
   elsif deli.length > 0 
   puts "You are #{deli.length} in line."
