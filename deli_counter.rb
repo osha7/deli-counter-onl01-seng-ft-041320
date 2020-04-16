54b264477c1b54d60c2932596f60fa3d6cd7cdc3
Osha G <ogroetz@gmail.com>
2020-04-16 04:23:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ def now_serving(deli)
   if deli.length == 0 
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{name}."
+    puts "Currently serving #{deli[0]}."
   end
     
     
